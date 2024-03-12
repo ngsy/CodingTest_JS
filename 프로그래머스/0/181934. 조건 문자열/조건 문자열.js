e75a40c6c44cf4e820d94ea2e2e2579fee942711
Soo Yeong Nam Goong <72487120+ngsy@users.noreply.github.com>
2024-03-12 18:23:50 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -2,13 +2,13 @@ function solution(ineq, eq, n, m) {
     let answer = 0;
     
     if(ineq === '>'){
-        if(eq === '=') return answer=n >= m? 1:0;
-        answer=n > m? 1:0    
+        if(eq === '=') return answer= Number(n >= m)
+        answer= Number(n > m)
     }
 
     if(ineq === '<'){
-        if(eq === '=') return answer = m >= n? 1:0;
-        answer = m > n? 1:0   
+        if(eq === '=') return answer = Number(m >= n)
+        answer = Number(m > n)
     }
         
     return answer;
