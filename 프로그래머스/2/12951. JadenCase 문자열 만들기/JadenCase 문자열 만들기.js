536fde378aaf2db81b1c62d48db7f110c8fb486a
Soo Yeong Nam Goong <72487120+ngsy@users.noreply.github.com>
2024-03-16 12:38:35 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,3 +1,8 @@
+/**JadenCase 문자열 만들기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12951
+ * 
+ *
+ */
 function solution(s) {
     let str = s.split(" ").map((val) => {
         let temp = val.toLowerCase();
