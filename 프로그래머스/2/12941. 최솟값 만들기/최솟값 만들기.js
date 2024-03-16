34c06728ef05a5d8c40e3aa14a1e6a08320080a8
Soo Yeong Nam Goong <72487120+ngsy@users.noreply.github.com>
2024-03-16 13:11:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.77 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,6 @@
 function solution(A,B){
-    
-    const sortB = B.sort((a,b) => b-a);// 내림차순
-    const answer = A.sort((a,b) => a-b).reduce((acc,cur,idx) =>  acc + (cur *sortB[idx])
+    B.sort((a,b) => b-a);
+    const answer = A.sort((a,b) => a-b).reduce((acc,cur,idx) =>  acc + (cur *B[idx])
     ,0)
     
     
