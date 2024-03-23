67e72f7bc11bf16be9714bd6bc8331d5a57350dc
Soo Yeong Nam Goong <72487120+ngsy@users.noreply.github.com>
2024-03-24 00:41:59 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.85 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let count = 0;
+    let zeroCnt =0;
+    let c = 0;
+    let zeroSize;
+    let temp = s;
+    while(c !== '1'){
+        //0의 개수 구하기
+        zeroSize = temp.split('').filter((el) => el !== '1').length;
+        //x의 길이 이진법 변환
+        c = temp.replaceAll(0,'').length.toString(2);    
+        temp = c;
+        count++;
+        zeroCnt+=zeroSize;
+    }
+    return [count,zeroCnt];
+}
