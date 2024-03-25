a206dea3bacfcd03e37419a1adb0dd385e41469b
Soo Yeong Nam Goong <72487120+ngsy@users.noreply.github.com>
2024-03-25 23:22:25 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,7 +1,14 @@
-//짝수 대문자 홀수 소문자
+/**이상한 문자 만들기 Lv.1
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12930
+ *
+ * 
+ *
+ * 
+ */
 function solution(s) {
     const answer = s.split(' ').reduce((acc,cur) => {
         const word = cur.split('').map((val,index) => index % 2 === 0? val.toUpperCase() : val.toLowerCase()).join('');
+        
         acc.push(word);
         return acc 
     },[]);
