f96c06853a6a88b61e929bd91bc66bda09b6cd69
Soo Yeong Nam Goong <72487120+ngsy@users.noreply.github.com>
2024-03-26 00:38:55 +0900

    [level 2] Title: 숫자의 표현, Time: 2.04 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,18 +1,31 @@
-//fill을 공백으로 선언하면 undefined로 채워짐
-function solution(n) {
-    // const numArr = new Array(n).fill().map((_,idx) => idx+1); 
+/**숫자의 표현 Lv.2
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12924
+ *
+ *
+ *
+ *
+ */
+function solution(n) {    
+    const numArr = new Array(n).fill().map((_,idx) => idx+1);
     let sum =0;
     let count = 0;
-    
-    for(let i =1; i <= n;i++){
+    // for(let i =1; i <= n;i++){
+    //     sum =0;
+    //   for(let j = i; j <= n; j++){
+    //       sum+=j;
+    //     if(sum === n) ++count;
+    //     if(sum > n) break;   
+    //   }
+    // }
+    numArr.forEach((val) => {
         sum =0;
-      for(let j = i; j <= n; j++){
+      for(let j = val; j <= n; j++){
           sum+=j;
         if(sum === n) ++count;
         if(sum > n) break;   
       }
-    }
+    })
     
-    let answer = count;
-    return answer;
+  
+    return count;
 }
